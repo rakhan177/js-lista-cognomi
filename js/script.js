b0018a6f4d6f3465c3e4010d078ed20e56f8d017
Matteo <metboolean@gmail.com>
2020-12-16 17:21:14 +0100

    lista

--- js/script.js
+++ js/script.js
@@ -10,6 +10,16 @@ console.log(cognomeUtente);
 var cognomiVari = ['Busi', 'Doria', 'Orietti', 'Zampelli', 'Raffelli', 'Secchi'];
 console.log(cognomiVari);
 /*con push aggiungo uno o + elementi alla fine di un arry,quindi:
-nome_array.push(variabile/'stringa')*/
+nome_array.push(variabile/'stringa')
+*/
 cognomiVari.push(cognomeUtente);
 console.log(cognomiVari);
+/*sort converte gli elementi in stringhe,quindi, in caso di numeri,saranno
+convertiti in stringhe e l' ordine sarà falsato perchè sort ordina controllando
+solo la prima cifra/numero; mentre in caso di lettere la maiuscola avrà la precedenza */
+cognomiVari.sort();
+console.log(cognomiVari);
+// creo ciclo for per inserimento lista in HTML
+for (var i = 0; i < cognomiVari.lenght; i++){
+  lista_cognomi.innerHTML += '<li>' + cognomiVari[i] + '</li>';
+}

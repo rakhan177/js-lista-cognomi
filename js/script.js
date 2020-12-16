fb0039380f874c5ee01ec65aab13e89d8343ab14
Matteo <metboolean@gmail.com>
2020-12-16 16:07:40 +0100

    hgr

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('ciao');
+// creo variabili
+var benvenuto = document.getElementById('benvenuto');
+var lista_cognomi = document.getElementById('lista_cognomi');
+
+// cognome utente
+var cognomeUtente = prompt('Scriva il suo cognome');
+console.log(cognomeUtente);
+// creo array con cognomi vari
+var cognomiVari = ['Busi', 'Doria', 'Orietti', 'Zampelli', 'Raffelli', 'Secchi'];
+console.log(cognomiVari);
